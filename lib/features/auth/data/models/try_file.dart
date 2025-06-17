d5af39be3c92597e865af7fefaaf9a02f765914f
rudiansyah <rudiansyahether@gmail.com>
2025-06-17 09:33:03 +0700

    ✨ (try_file.dart): add Product model with fromJson and toJson methods to handle product data serialization and deserialization

---
+++ lib/features/auth/data/models/try_file.dart
@@ -0,0 +1,31 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final String description;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'].toDouble(),
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'description': description,
+    };
+  }
+}
